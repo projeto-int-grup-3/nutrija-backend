0f1ee3de653df9efdc2c8bf660342ba3d2d29284
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-10-08 10:00:07 -0300

    cria serviços DevService e ProdService com configurações de conexão ao banco de dados

---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_nutrija',
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
